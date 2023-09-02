f5b9c90e631607d878846a8ae417ff46492336a5
Trilok_rana <ranatrilok1234@gmail.com>
2023-09-02 20:03:39 +0530

    commit changes within the files

---
+++ src/app/api/userTasks/route.js
@@ -0,0 +1,27 @@
+
+
+import dbConnect from "../../db";
+import Task from "../../model/task";
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  if (req.method === "POST") {
+    try {
+      const task = new Task({ task: req.body.task });
+      await task.save();
+      res.status(201).json(task);
+    } catch (error) {
+      res.status(500).json({ error: "Error creating task" });
+    }
+  } else if (req.method === "GET") {
+    try {
+      const tasks = await Task.find();
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(500).json({ error: "Error fetching tasks" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
---
+++ src/model/Task.js
@@ -0,0 +1,16 @@
+
+
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.models.Task || mongoose.model("Task", TaskSchema);
