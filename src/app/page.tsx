647fc85b16fcb22d77de7a59b74d1efc405d7e43
Trilok_rana <ranatrilok1234@gmail.com>
2023-09-02 22:33:28 +0530

    remove commment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  // Define authOptions before using it
+
   const authOptions = {
     secret: "process.env.NEXTAUTH_SECRET", 
     session: {
