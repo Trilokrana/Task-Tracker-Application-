46a76fd02cdfcc21b6462884b725823bf455f7e7
Trilok_rana <ranatrilok1234@gmail.com>
2023-09-04 18:49:27 +0530

    change file data

--- src/app/api/userTasks/route.js
+++ src/app/api/userTasks/route.js
@@ -1,21 +1,26 @@
 
-import connectDB from '../../db';
-
-connectDB();
+import dbConnect from "../../db";
+import Task from "../../models/task";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  await dbConnect();
+
+  if (req.method === "POST") {
     try {
-      const { task } = req.body;
-
-      const newTask = await YourTaskModel.create({ task });
-
-      res.status(201).json(newTask);
+      const task = new Task({ task: req.body.task });
+      await task.save();
+      res.status(201).json(task);
     } catch (error) {
-      console.error('Error creating task:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: "Error creating task" });
+    }
+  } else if (req.method === "GET") {
+    try {
+      const tasks = await Task.find();
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(500).json({ error: "Error fetching tasks" });
     }
   } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
