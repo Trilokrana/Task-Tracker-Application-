96cd39ff98f98453273b83988afa0e62430b973b
Trilok_rana <ranatrilok1234@gmail.com>
2023-09-06 01:10:50 +0530

    remove authOptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation';
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
 
-  if (session) redirect("/dashboard");
   return (
     <main>
       <Login />
